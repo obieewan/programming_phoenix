bd9525a68ccaa60b5e48edc818376fb940539861
Obie <obie.tizon@gmail.com>
2023-01-20 20:06:52 +0800

    getting to know more about the flow of elixir in templates

--- rumbl/lib/rumbl_web/router.ex
+++ rumbl/lib/rumbl_web/router.ex
@@ -17,7 +17,10 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser
 
+    get "/users", UserController, :index
+    get "/users/:id", UserController, :show
     get "/", PageController, :index
+
   end
 
   # Other scopes may use custom stacks.
