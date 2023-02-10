fd5683634d2891e38acde610e810a0d8c267b808
Obie <obie.tizon@gmail.com>
2023-02-10 21:44:42 +0800

    add user registration

--- rumbl/lib/rumbl/accounts.ex
+++ rumbl/lib/rumbl/accounts.ex
@@ -33,4 +33,13 @@ defmodule Rumbl.Accounts do
     |> Repo.insert()
   end
 
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
 end
